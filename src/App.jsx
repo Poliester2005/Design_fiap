e834b739d73da73810da5c65bf0f8a685fc5fb83
Eduardo Pielich <107966984+Poliester2005@users.noreply.github.com>
2023-09-19 11:30:32 -0300

    commit 1

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+      <div className='container'>
+        <section className='estrutura'>
+          <article className='box'>
+            <h1>Nossa estrutura</h1>
+            <img src="http://lorempixel.com.br/350/200" alt="" />
+            <p>
+              aduiv gsauyv djuaw s\wmhcvhkasv cjksd vjlsd vz\sz dvlu slkh us hdv
+            </p>
+            <a href="#">Veja mais</a>
+          </article>
+          <article className='box'>
+            <h1>Nossa estrutura</h1>
+            <img src="http://lorempixel.com.br/350/200/?1" alt="" />
+            <p>
+              aduiv gsauyv djuaw s\wmhcvhkasv cjksd vjlsd vz\sz dvlu slkh us hdv
+            </p>
+            <a href="#">Veja mais</a>
+          </article>
+        </section>
+
+        <section className="estrutura-card">
+          <h1>Criando Mixins</h1>
+          <div>
+            <article>
+              <p>
+                Lorem
+              </p>
+            </article>
+            <article>
+              <p>
+                Lorem
+              </p>
+            </article>
+            <article>
+              <p>
+                Lorem
+              </p>
+            </article>
+            <article>
+              <p>
+                Lorem
+              </p>
+            </article>
+            
+          </div>
+        </section>
+      </div >
+    </>
+  )
+}
+
+export default App
